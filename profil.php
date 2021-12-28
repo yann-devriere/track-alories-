31ed7cd943a95cba2a8e180a5bac174df0422f2b
Yann.Devriere <yann.devriere@gmail.com>
2021-12-28 19:56:28 +0100

    detail3

--- profil.php
+++ profil.php
@@ -118,16 +118,16 @@ ton IMC est de <?php include ('tools/calculator.php');?>
       </div>
       <div class="modal-body">
 
-      <div class="h4">
-        Tu mesures : <?php echo $_SESSION['taille']?> pour <?php echo $_SESSION['poids']?> kg.
+      <div class="h4 text-info">
+        Tu mesures : <?php echo $_SESSION['taille']?> m pour <?php echo $_SESSION['poids']?> kg.
       </div>
 
       <form class="formUpdate" method="POST" action="./class/updateUser.php"> <h3>Un changement physique ?</h3>
             <div class="innerMenu">
              <p>Nouveau poids</p>
-                <input type="number" name="newPoids" required placeholder="Nouveau poids en kg"></input>
+                <input class="form-control" type="number" name="newPoids" required placeholder="Nouveau poids en kg"></input>
              <p class="mt-5">Nouvelle taille</p>
-                <input type="decimal" name="newTaille"  required placeholder="Nouvelle taille en m"></input>
+                <input class="form-control" type="decimal" name="newTaille"  required placeholder="Nouvelle taille en m"></input>
                 <div class="btnSpot">
                 <button type="submit" name="btnUpdate" class=" btn btn-success mt-1 btnUpdate">Modifier</button>
                 </div>
